bdc88079d2509f8adafbc4aec93b220ca24a7495
Ryan.O'Kuinghttons <rokuingh@gmail.com>
2014-09-18 14:44:41 -0600

    add content_type and remove some comments

--- ESMF_atomfeed/esmfcimfeed/eca/urls.py
+++ ESMF_atomfeed/esmfcimfeed/eca/urls.py
@@ -4,9 +4,7 @@ from eca.models import ESMFAtomFeed
 import eca.views as views
 
 urlpatterns = patterns('',
-    # ex: Atom Feed stuff
     url(r'^feed/$', ESMFAtomFeed()),
-    #url(r'^feed/(?P<doc>[^/]+)/$', ESMFAtomFeed()),
 
     # ex: /eca/feed/CESM_Component.xml
     url(r'^feed/(?P<doc>[^/]+)/$', views.show_doc),
