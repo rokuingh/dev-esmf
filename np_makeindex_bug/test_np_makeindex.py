774762ebfe91148a450ec49d08c355a66a887b39
Ryan.O'Kuinghttons <rokuingh@gmail.com>
2015-03-27 13:52:14 -0600

    clean up the np_makeindex test

--- np_makeindex_bug/test_np_makeindex.py
+++ np_makeindex_bug/test_np_makeindex.py
@@ -1,58 +1,26 @@
 # test a possible numpy versioning issue in ESMP
 
-def make_index_3d( nx, ny, nz ):
-    '''
-    Return an array, of size (nz, ny, nx) that is
-    an index, ie runs from 0 to (nx*ny*nz)-1
-    '''
-    import numpy as np
-
-    return np.arange( nx*ny*nz ).reshape( nz, ny, nx )
-
-
 def test_makeindex():
-
     import numpy as np
 
-    print np.__version__
+    def make_index_3d(nx, ny, nz):
+        '''
+        Return an array, of size (nz, ny, nx) that is
+        an index, ie runs from 0 to (nx*ny*nz)-1
+        '''
 
-    [x,y,z] = [0,1,2]
+        return np.arange(nx * ny * nz).reshape(nz, ny, nx)
 
-    [i,j,k] = [2,2,2]
+    print "numpy version {0}".format(np.__version__)
 
-    pts = make_index_3d(i,j,k)
+    [i, j, k] = [2, 2, 2]
 
-    Xarray = np.empty(i * j * k)
-    Yarray = np.empty(i * j * k)
-    Zarray = np.empty(i * j * k)
+    pts = make_index_3d(i, j, k)
+
+    array = np.empty(i * j * k)
 
     for i0 in range(i):
-        Xarray[pts[i0, :, :]] = float(i0)
+        array[pts[:, :, i0]] = float(i0)
 
-    # for i1 in range(j):
-    #     Yarray[pts[:, i1, :]] = float(i1)
-    #
-    # for i2 in range(k):
-    #     Zarray[pts[i2, :, :]] = float(i2)
-
-    correct = True
     for i0 in range(i):
-        if (Xarray[pts[i0, :, :]] != float(i0)).any():
-            correct = False
-            print "FAIL - Xarray[:,:," + str(i0) + "] = " + str(Xarray[pts[i0, :, :]])
-
-    # for i1 in range(j):
-    #     if (Yarray[pts[:, i1, :]] != float(i1)).any():
-    #         correct = False
-    #         print "FAIL - Yarray[:," + str(i1) + ",:] = " + str(Yarray[pts[:, i1, :]])
-    #
-    # for i2 in range(k):
-    #     if (Zarray[pts[i2, :, :]] != float(i2)).any():
-    #         correct = False
-    #         print "FAIL - Zarray[" + str(i2) + ",:,:] = " + str(Zarray[pts[i2, :, :]])
-
-
-    assert(correct)
-
-
-
+        assert ((array[pts[:, :, i0]] == float(i0)).all())
