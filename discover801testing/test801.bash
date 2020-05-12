7d9917924c813b705bd1f55d3b0d0c9bc6c448be
Ryan.O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2020-05-11 20:38:43 -0700

    revert to separate clone for every platform

--- discover801testing/test801.bash
+++ discover801testing/test801.bash
@@ -154,41 +154,28 @@ commonesmfvars="export ESMF_OS=Linux; export ESMF_ABI=64; export ESMF_SITE=defau
 
 
 # all cases (disk quota issues on discover)
-# declare -a LibTests=("gfortran481" "intel17" "intel1803" "intel1805" "nag62" "pgi14" "pgi17")
-
-# gfortran
-declare -a LibTests=("gfortran481")
-
-# intel
-# declare -a LibTests=("intel17" "intel1803" "intel1805")
-
-# nagpgi
-# declare -a LibTests=("nag62" "pgi14" "pgi17")
-
-# test with just one
-# declare -a LibTests=("intel17mvapich2lib")
-
+# declare -a LibTests=("gfortran481mpiunilib" "gfortran481mvapich2lib" "gfortran481openmpilib" "intel17mvapich2lib" "intel1803openmpilib" "intel1805mpiunilib" "intel1805impilib" "nag62mpiunilib" "pgi14mvapich2lib" "pgi17mpiunilib" "pgi17openmpilib")
 
 # gfortran481
-declare -a gfortran481=("gfortran481mpiunilib" "gfortran481mvapich2lib" "gfortran481openmpilib")
+declare -a LibTests=("gfortran481mpiunilib" "gfortran481mvapich2lib" "gfortran481openmpilib")
 
 # intel17
-declare -a intel17=("intel17mvapich2lib")
+# declare -a LibTests=("intel17mvapich2lib")
 
 # intel1803
-declare -a intel1803=("intel1803openmpilib")
+# declare -a LibTests=("intel1803openmpilib")
 
 # intel1805
-declare -a intel1805=("intel1805mpiunilib" "intel1805impilib")
+# declare -a LibTests=("intel1805mpiunilib" "intel1805impilib")
 
 # nag62
-declare -a nag62=("nag62mpiunilib")
+# declare -a LibTests=("nag62mpiunilib")
 
 # pgi14
-declare -a pgi14=("pgi14mvapich2lib")
+# declare -a LibTests=("pgi14mvapich2lib")
 
 # pgi17
-declare -a pgi17=("pgi17mpiunilib" "pgi17openmpilib")
+# declare -a LibTests=("pgi17mpiunilib" "pgi17openmpilib")
 
 # g and O
 declare -a Mode=("g" "O")
@@ -206,56 +193,37 @@ workdir=$NOBACKUP/discovertesting801
 RUNDIR=$(python $scriptdir/run_id.py $workdir 2>&1)
 mkdir $RUNDIR
 
-# function to replace nameref in >4.3 bash (thank you discover)
-submitcombos ()
-{
-  # nameref magic
-  local testproxy="${1}[@]"
-  local testproxyname="${1}"
-  local testproxyvals=("${!testproxy}")
-
-  # set up test parameters
-  TESTPROXYDIR=$RUNDIR/$testproxyname
-  mkdir $TESTPROXYDIR/logs
-  logdir="export LOGDIR=$TESTPROXYDIR/logs"
-  esmfdir="export ESMF_DIR=$TESTPROXYDIR/esmf"
-  
-
-  # actually iterate the combos and submit the jobs
-  for combo in "${testproxyvals[@]}"; do
-    # set the case specific variables, via function
-    $combo
-
-    for mode in "${Mode[@]}"; do
-      # set up test parameters
-      esmfbopt="export ESMF_BOPT=$mode"
-    
-      # sbatch --export,--get-user-env doesn't work, so manually set the environment
-      sed "s&%testname%&$combo-$mode&g; s&%homedir%&$homedir&g; s&%logdir%&$logdir&g; s&%modules%&$modules&g; s&%esmfdir%&$esmfdir&g; s&%commonesmfvars%&$commonesmfvars&g; s&%esmfenv%&$esmfenv&g; s&%esmfbopt%&$esmfbopt&g" $scriptdir/esmftest.slurm > esmftest-$combo-$mode.slurm
-  
-      # run the test
-      echo "sbatch esmftest-$combo-$mode.slurm"
-      # sbatch esmftest--$combo-$mode.slurm
-    done
-  done
-}
-
 for test in "${LibTests[@]}"; do
   cd $RUNDIR
-  
-  # create test directory
-  TESTDIR=$RUNDIR/$test
-  mkdir $TESTDIR
-  cd $TESTDIR
-  echo $TESTDIR
-  
-  # clone esmf and checkout appropriate tag
-  echo "cloning esmf..."
-  git clone git@github.com:esmf-org/esmf.git >/dev/null 2>&1
-  cd esmf
-  git checkout ESMF_8_0_1_beta_snapshot_13
-  cd ..
 
-  submitcombos $test
+  # activate the specific test variables
+  $test
 
+  for mode in "${Mode[@]}"; do
+    # create test directory
+    TESTDIR=$RUNDIR/$test-$mode
+    mkdir $TESTDIR
+    cd $TESTDIR
+    echo $TESTDIR
+    
+    # set up test parameters
+    esmfdir="export ESMF_DIR=$TESTDIR/esmf"
+    esmfbopt="export ESMF_BOPT=$mode"
+    logdir="export LOGDIR=$TESTDIR/logs"
+    mkdir $TESTDIR/logs
+
+    # clone esmf and checkout appropriate tag
+    echo "cloning esmf..."
+    git clone git@github.com:esmf-org/esmf.git >/dev/null 2>&1
+    cd esmf
+    git checkout ESMF_8_0_1_beta_snapshot_13
+    cd ..
+
+    # sbatch --export,--get-user-env doesn't work, so manually set the environment
+    sed "s&%testname%&$test-$mode&g; s&%homedir%&$homedir&g; s&%logdir%&$logdir&g; s&%modules%&$modules&g; s&%esmfdir%&$esmfdir&g; s&%commonesmfvars%&$commonesmfvars&g; s&%esmfenv%&$esmfenv&g; s&%esmfbopt%&$esmfbopt&g" $scriptdir/esmftest.slurm > esmftest-$test-$mode.slurm
+  
+    # run the test
+    echo "sbatch esmftest-$test-$mode.slurm"
+    sbatch esmftest-$test-$mode.slurm
+  done
 done
